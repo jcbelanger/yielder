1ac3d007d6861a139b7b593992d5e31b05959ebc
Joshua Belanger <jcbelanger@users.noreply.github.com>
2018-02-28 01:31:45 -0500

    Fix leak in touchPaths

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
+Rx.Observable.prototype.slidingWindow = function(n) {
+	return Rx.Observable.range(0, n)
+		.map(i => this.skip(i))
+		.zipAll();
+}
+
 const drawing = document.getElementById("drawing");
 const svgNS = "http://www.w3.org/2000/svg";
 
-const mouseLines = Rx.Observable.fromEvent(drawing, "mousedown")
-	.map(mouseDown => Rx.Observable.fromEvent(document, "mousemove")
-		.startWith(mouseDown)
-		.do(mouseEvent => mouseEvent.preventDefault())
-		.takeUntil(Rx.Observable.fromEvent(document, "mouseup")
-			.do(mouseUp => mouseUp.preventDefault())
-		)
-		.map(mouseEvent => toSvgPoint(drawing, mouseEvent))
-	);
+const mouseDowns = Rx.Observable.fromEvent(drawing, "mousedown");
+const mouseMoves = Rx.Observable.fromEvent(document, "mousemove");
+const mouseUps = Rx.Observable.fromEvent(document, "mouseup");
 
-const touchStops = Rx.Observable.fromEvent(drawing, "touchend")
-	.merge(Rx.Observable.fromEvent(drawing, "touchcancel"))
+const mousePaths = mouseDowns.map(mouseDown => mouseMoves
+	.startWith(mouseDown)
+	.do(mouseEvent => mouseEvent.preventDefault())
+	.map(mouseEvent => toSvgPoint(drawing, mouseEvent))
+	.takeUntil(mouseUps.do(mouseEvent => mouseEvent.preventDefault()))
+);
+
+const touchStarts = Rx.Observable.fromEvent(drawing, "touchstart");
+const touchMoves = Rx.Observable.fromEvent(drawing, "touchmove");
+const touchEnds = Rx.Observable.fromEvent(drawing, "touchend");
+const touchCancels = Rx.Observable.fromEvent(drawing, "touchcancel");
+
+const touchPaths = touchStarts
+	.merge(touchMoves)
+	.do(touchEvent => touchEvent.preventDefault())
 	.mergeMap(touchEvent => Rx.Observable.from(touchEvent.changedTouches))
+	.groupBy(
+		touch => touch.identifier,
+		touch => toSvgPoint(drawing, touch),
+		group => touchEnds
+			.merge(touchCancels)
+			.do(touchEvent => touchEvent.preventDefault())
+			.mergeMap(touchEvent => Rx.Observable.from(touchEvent.changedTouches))
+			.find(touch => touch.identifier === group.key)
+	)
 
-const touchLines = Rx.Observable.fromEvent(drawing, "touchstart")
-	.mergeMap(touchStart => Rx.Observable.fromEvent(drawing, "touchmove")
-		.startWith(touchStart)
-		.do(touchEvent => touchEvent.preventDefault())
-		.mergeMap(touchEvent => Rx.Observable.from(touchEvent.changedTouches))
-		.groupBy(
-			touch => touch.identifier,
-			touch => toSvgPoint(drawing, touch),
-			group => touchStops.find(touch => touch.identifier === group.key)
-		)
-	);
+const rawPaths = mousePaths.merge(touchPaths);
 
-
-const lines = mouseLines.merge(touchLines);
-
-lines.subscribe(line => {
-	const poly = document.createElementNS(svgNS, "polyline");
-	poly.classList.add("drawing");
-	drawing.appendChild(poly);
-	line.subscribe(
-		point => poly.points.appendItem(point),
-		null,
-		() => drawing.removeChild(poly)
-	);
-});
+logPaths(rawPaths);
+// logPaths(simplePaths);
+renderPaths(rawPaths, "raw-path");
+// renderPaths(simplePaths, "simple-path");
 
 
 function toSvgPoint(svg, point) {
@@ -48,3 +51,33 @@ function toSvgPoint(svg, point) {
 	pt.y = point.clientY;
 	return pt.matrixTransform(svg.getScreenCTM().inverse())
 };
+
+function toClientPoint(svg, point) {
+	return point.matrixTransform(svg.getScreenCTM());
+}
+
+function triangeArea( [{x:x1, y:y1}, {x:x2, y:y2}, {x:x3, y:y3}] ) {
+	return ((x1 - x3) * (y2 - y1) - (x1 - x2) * (y3 - y1)) / 2;
+}
+
+
+function renderPaths(paths, pathClass, removeAfter=true) {
+	paths.subscribe(path => {
+		const poly = document.createElementNS(svgNS, "polyline");
+		poly.classList.add(pathClass);
+		drawing.appendChild(poly);
+		path.subscribe(
+			point => poly.points.appendItem(point),
+			error => console.error(error),
+			() => removeAfter ? drawing.removeChild(poly) : ""
+		);
+	});
+}
+
+function logPaths(paths) {
+	paths.subscribe(path => path.subscribe(
+		point => console.log(point),
+		e => console.error(e),
+		() => console.log('complete')
+	));
+}
